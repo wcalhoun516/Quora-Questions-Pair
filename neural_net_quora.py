5a809aae44d18e4f7b7fc0b9523ab21207dbd6ea
wcalhoun516 <willcalhoun516@gmail.com>
2017-06-05 17:34:46 -0400

    Update neural_net_quora.py

--- neural_net_quora.py
+++ neural_net_quora.py
@@ -2,7 +2,7 @@ trainDF, valDF = load_train(train_file='../downloads/train.csv')
 train_question1, train_question2, val_question1, val_question2 = clean_up(trainDF=trainDF, valDF=valDF)
 BagOfWordsExtractor, X_train, x_val, y_train, y_val = BOW()
 graph = tensor(n_epochs=25, length=len(X_train))
-testDF = load_test(BagofWordsExtractor=BagofWordsExtractor,test_file='../downloads/test.csv', test_size=0.2)
+testDF = load_test(BagOfWordsExtractor=BagofWordsExtractor,test_file='../downloads/test.csv', test_size=0.2)
 test = process_test
 predictions = tensor_load(length=len(test), prediction_length=10000)
 output = convert_to_output(predictions, len(test),to_csv=1)
