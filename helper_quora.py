51f197205bd634dd84c4124428697cccdbc4dbb9
wcalhoun516 <willcalhoun516@gmail.com>
2017-06-06 13:17:15 -0400

    Update helper_quora.py

--- helper_quora.py
+++ helper_quora.py
@@ -53,128 +53,6 @@ from IPython.display import clear_output, Image, display, HTML
 
 
 
-def calculate_vif_(X, thresh=5.0):
-    from statsmodels.stats.outliers_influence import variance_inflation_factor 
-    variables = list(X.columns)
-    dropped=True
-    while dropped:
-        dropped=False
-        vif = [variance_inflation_factor(X[variables].values, ix) for ix in range(X[variables].shape[1])]
-        #print(vif)
-        maxloc = vif.index(max(vif))
-        
-        if max(vif) > thresh:
-            #print('dropping \'' + X[variables].columns[maxloc] + '\' at index: ' + str(maxloc))
-            variables.pop(maxloc)
-            dropped=True
-
-    #print('Remaining variables:')
-    #print(X[variables])
-    return X[variables]
-
-
-
-def scale_features_(X,round_digit=3):
-    from sklearn_pandas import DataFrameMapper
-    import numpy as np
-    mapper = DataFrameMapper([(X.columns, StandardScaler())])
-    scaled_features = np.round(mapper.fit_transform(X.copy()), round_digit)
-    scaled_features_df = pd.DataFrame(scaled_features, index=X.index, columns=X.columns)
-
-
-
-def word_vector(word_vector_file='../downloads/GoogleNews-vectors-negative300.bin'):
-    wordVectorStartTime = time.time()
-    print('Indexing word vectors')
-
-    word2vec = KeyedVectors.load_word2vec_format(word_vector_file, \
-        binary=True)
-    print('Found %s word vectors of word2vec' % len(word2vec.vocab))
-    wordVectorDurationInMinutes = (time.time()-wordVectorStartTime)/60.0
-    print("feature extraction took %.2f minutes" % (wordVectorDurationInMinutes))
-
-
-
-
-
-def text_to_wordlist(text, stop_words, remove_stop_words=True, stem_words=False):
-    # Clean the text, with the option to remove stop_words and to stem words.
-
-    # Clean the text
-    text = re.sub(r"[^A-Za-z0-9]", " ", text)
-    text = re.sub(r"what's", "", text)
-    text = re.sub(r"What's", "", text)
-    text = re.sub(r"\'s", " ", text)
-    text = re.sub(r"\'ve", " have ", text)
-    text = re.sub(r"can't", "cannot ", text)
-    text = re.sub(r"n't", " not ", text)
-    text = re.sub(r"I'm", "I am", text)
-    text = re.sub(r" m ", " am ", text)
-    text = re.sub(r"\'re", " are ", text)
-    text = re.sub(r"\'d", " would ", text)
-    text = re.sub(r"\'ll", " will ", text)
-    text = re.sub(r"60k", " 60000 ", text)
-    text = re.sub(r" e g ", " eg ", text)
-    text = re.sub(r" b g ", " bg ", text)
-    text = re.sub(r"\0s", "0", text)
-    text = re.sub(r" 9 11 ", "911", text)
-    text = re.sub(r"e-mail", "email", text)
-    text = re.sub(r"\s{2,}", " ", text)
-    text = re.sub(r"quikly", "quickly", text)
-    text = re.sub(r" usa ", " America ", text)
-    text = re.sub(r" USA ", " America ", text)
-    text = re.sub(r" u s ", " America ", text)
-    text = re.sub(r" uk ", " England ", text)
-    text = re.sub(r" UK ", " England ", text)
-    text = re.sub(r"india", "India", text)
-    text = re.sub(r"switzerland", "Switzerland", text)
-    text = re.sub(r"china", "China", text)
-    text = re.sub(r"chinese", "Chinese", text) 
-    text = re.sub(r"imrovement", "improvement", text)
-    text = re.sub(r"intially", "initially", text)
-    text = re.sub(r"quora", "Quora", text)
-    text = re.sub(r" dms ", "direct messages ", text)  
-    text = re.sub(r"demonitization", "demonetization", text) 
-    text = re.sub(r"actived", "active", text)
-    text = re.sub(r"kms", " kilometers ", text)
-    text = re.sub(r"KMs", " kilometers ", text)
-    text = re.sub(r" cs ", " computer science ", text) 
-    text = re.sub(r" upvotes ", " up votes ", text)
-    text = re.sub(r" iPhone ", " phone ", text)
-    text = re.sub(r"\0rs ", " rs ", text) 
-    text = re.sub(r"calender", "calendar", text)
-    text = re.sub(r"ios", "operating system", text)
-    text = re.sub(r"gps", "GPS", text)
-    text = re.sub(r"gst", "GST", text)
-    text = re.sub(r"programing", "programming", text)
-    text = re.sub(r"bestfriend", "best friend", text)
-    text = re.sub(r"dna", "DNA", text)
-    text = re.sub(r"III", "3", text) 
-    text = re.sub(r"the US", "America", text)
-    text = re.sub(r"Astrology", "astrology", text)
-    text = re.sub(r"Method", "method", text)
-    text = re.sub(r"Find", "find", text) 
-    text = re.sub(r"banglore", "Banglore", text)
-    text = re.sub(r" J K ", " JK ", text)
-    
-    # Remove punctuation from text
-    text = ''.join([c for c in text if c not in punctuation])
-    
-    # Optionally, remove stop words
-    if remove_stop_words:
-        text = text.split()
-        text = [w for w in text if not w in stop_words]
-        text = " ".join(text)
-    
-    # Optionally, shorten words to their stems
-    if stem_words:
-        text = text.split()
-        stemmer = SnowballStemmer('english')
-        stemmed_words = [stemmer.stem(word) for word in text]
-        text = " ".join(stemmed_words)
-    
-    # Return a list of words
-    return(text)
 
 
 
@@ -539,8 +417,132 @@ def convert_to_output(predictions, length, to_csv=0, filename='Submission'):
         return output
 
 
+def calculate_vif_(X, thresh=5.0):
+    from statsmodels.stats.outliers_influence import variance_inflation_factor 
+    variables = list(X.columns)
+    dropped=True
+    while dropped:
+        dropped=False
+        vif = [variance_inflation_factor(X[variables].values, ix) for ix in range(X[variables].shape[1])]
+        #print(vif)
+        maxloc = vif.index(max(vif))
+        
+        if max(vif) > thresh:
+            #print('dropping \'' + X[variables].columns[maxloc] + '\' at index: ' + str(maxloc))
+            variables.pop(maxloc)
+            dropped=True
+
+    #print('Remaining variables:')
+    #print(X[variables])
+    return X[variables]
 
 
+
+def scale_features_(X,round_digit=3):
+    from sklearn_pandas import DataFrameMapper
+    import numpy as np
+    mapper = DataFrameMapper([(X.columns, StandardScaler())])
+    scaled_features = np.round(mapper.fit_transform(X.copy()), round_digit)
+    scaled_features_df = pd.DataFrame(scaled_features, index=X.index, columns=X.columns)
+
+
+
+def word_vector(word_vector_file='../downloads/GoogleNews-vectors-negative300.bin'):
+    wordVectorStartTime = time.time()
+    print('Indexing word vectors')
+
+    word2vec = KeyedVectors.load_word2vec_format(word_vector_file, \
+        binary=True)
+    print('Found %s word vectors of word2vec' % len(word2vec.vocab))
+    wordVectorDurationInMinutes = (time.time()-wordVectorStartTime)/60.0
+    print("feature extraction took %.2f minutes" % (wordVectorDurationInMinutes))
+
+
+
+
+
+def text_to_wordlist(text, stop_words, remove_stop_words=True, stem_words=False):
+    # Clean the text, with the option to remove stop_words and to stem words.
+
+    # Clean the text
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    text = re.sub(r"what's", "", text)
+    text = re.sub(r"What's", "", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"I'm", "I am", text)
+    text = re.sub(r" m ", " am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r"60k", " 60000 ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e-mail", "email", text)
+    text = re.sub(r"\s{2,}", " ", text)
+    text = re.sub(r"quikly", "quickly", text)
+    text = re.sub(r" usa ", " America ", text)
+    text = re.sub(r" USA ", " America ", text)
+    text = re.sub(r" u s ", " America ", text)
+    text = re.sub(r" uk ", " England ", text)
+    text = re.sub(r" UK ", " England ", text)
+    text = re.sub(r"india", "India", text)
+    text = re.sub(r"switzerland", "Switzerland", text)
+    text = re.sub(r"china", "China", text)
+    text = re.sub(r"chinese", "Chinese", text) 
+    text = re.sub(r"imrovement", "improvement", text)
+    text = re.sub(r"intially", "initially", text)
+    text = re.sub(r"quora", "Quora", text)
+    text = re.sub(r" dms ", "direct messages ", text)  
+    text = re.sub(r"demonitization", "demonetization", text) 
+    text = re.sub(r"actived", "active", text)
+    text = re.sub(r"kms", " kilometers ", text)
+    text = re.sub(r"KMs", " kilometers ", text)
+    text = re.sub(r" cs ", " computer science ", text) 
+    text = re.sub(r" upvotes ", " up votes ", text)
+    text = re.sub(r" iPhone ", " phone ", text)
+    text = re.sub(r"\0rs ", " rs ", text) 
+    text = re.sub(r"calender", "calendar", text)
+    text = re.sub(r"ios", "operating system", text)
+    text = re.sub(r"gps", "GPS", text)
+    text = re.sub(r"gst", "GST", text)
+    text = re.sub(r"programing", "programming", text)
+    text = re.sub(r"bestfriend", "best friend", text)
+    text = re.sub(r"dna", "DNA", text)
+    text = re.sub(r"III", "3", text) 
+    text = re.sub(r"the US", "America", text)
+    text = re.sub(r"Astrology", "astrology", text)
+    text = re.sub(r"Method", "method", text)
+    text = re.sub(r"Find", "find", text) 
+    text = re.sub(r"banglore", "Banglore", text)
+    text = re.sub(r" J K ", " JK ", text)
+    
+    # Remove punctuation from text
+    text = ''.join([c for c in text if c not in punctuation])
+    
+    # Optionally, remove stop words
+    if remove_stop_words:
+        text = text.split()
+        text = [w for w in text if not w in stop_words]
+        text = " ".join(text)
+    
+    # Optionally, shorten words to their stems
+    if stem_words:
+        text = text.split()
+        stemmer = SnowballStemmer('english')
+        stemmed_words = [stemmer.stem(word) for word in text]
+        text = " ".join(stemmed_words)
+    
+    # Return a list of words
+    return(text)
+
+
+    
+    
 def fetch_batch(data,label,epoch,n_batches,batch_index, batch_size):
     rnd.seed(epoch * n_batches + batch_index)          # not shown in the book
     indices = rnd.randint(len(data), size=batch_size)          # not shown
